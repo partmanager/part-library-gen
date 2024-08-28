bf721071863b784e763a9a74cb5f922685faba7e
sakoPO <partmanager.puritan246@passinbox.com>
2024-08-28 19:27:46 +0200

    Moved generator_map to separate file.

--- src/part_library_gen/symbol_generator.py
+++ src/part_library_gen/symbol_generator.py
@@ -1,6 +1,5 @@
-from .generators.opamp_generator import opamp_generator
-from .generators.rectangular_symbol_generator import rectangular_symbol_generator
 from .generators.default_generator import default_generator
+from .generators.generator_map import generator_map
 from .exporters.svg.svg_exporter import export as svg_exporter
 
 supported_pin_functions = ['In', 'Out', 'InAnalog', 'InDigital', 'InDigital;ActiveLow', 'InDigital;ActiveHigh',
@@ -38,10 +37,6 @@ def generate_file_name(component_data, generator_name):
 
 
 def generate(data):
-    generator_map = {'default': default_generator,
-                     'rectangle': rectangular_symbol_generator,
-                     'opamp': opamp_generator}
-
     validate(data)
     if 'symbol_generator' not in data:
         symbol = default_generator(data, {})
